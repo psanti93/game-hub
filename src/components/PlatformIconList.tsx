803eb74a232a6aa5d39a2219a161f0e2e7073ece
psanti93 <paulsantiago282@gmail.com>
2025-01-11 10:25:36 -0500

    Add component to display icons for each game

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,41 @@
+import {Platform} from "../hooks/useGames.ts";
+import {HStack, Icon} from "@chakra-ui/react";
+import { FaWindows, FaPlaystation, FaXbox, FaApple, FaLinux, FaAndroid} from "react-icons/fa";
+import {MdPhoneIphone} from "react-icons/md";
+import {SiNintendo} from "react-icons/si";
+import {BsGlobe} from "react-icons/bs";
+import {IconType} from "react-icons";
+interface Props {
+    platforms: Platform[]
+}
+
+const PlatformIconList = ({platforms}: Props) => {
+
+    // ea. platform object has the following name: Playstation slug: playstation (textual id)
+    // here we want to map the slug (textual id) to each platform component icons that we imported above
+   // [key: string] represents a index signature it takes care of the compilation error that we initially see when
+    // we put platform.slug in as
+    const iconMap: { [key: string]: IconType} = {
+        pc: FaWindows,
+        playstation: FaPlaystation,
+        xbox: FaXbox,
+        nintendo: SiNintendo,
+        mac: FaApple,
+        linux: FaLinux,
+        ios: MdPhoneIphone,
+        android: FaAndroid,
+        web: BsGlobe,
+    }
+
+
+
+    return(
+        <HStack marginY={1}>
+          {platforms.map((platform) =>
+                <Icon as={iconMap[platform.slug]} color={'gray.500'}/>
+          ) }
+        </HStack>
+    )
+}
+
+export default PlatformIconList;
