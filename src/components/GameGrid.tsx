6409ecf9999b4ace0b5f26590832b0dd004cd414
psanti93 <paulsantiago282@gmail.com>
2025-01-11 09:49:48 -0500

    add a grid to display out the cards, place the game card within that grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,5 +1,6 @@
-import {Text} from "@chakra-ui/react";
+import {SimpleGrid, Text} from "@chakra-ui/react";
 import useGames from "../hooks/useGames.ts";
+import GameCard from "../components/GameCard.tsx";
 
 
 const GameGrid = () => {
@@ -8,9 +9,12 @@ const GameGrid = () => {
     return(
         <>
         {error && <Text>{error}</Text>}
-        <ul>
-            {games.map(game=><li key={game.id}>{game.name}</li>)}
-        </ul>
+        {/* for the columns we're determining the size based on a device   */}
+        <SimpleGrid columns={{sm:1, md: 2, lg:3, xl:5}} padding={'10px'} spacing={10}>
+            {games.map(game=>
+                <GameCard key={game.id} game={game}/>
+            )}
+        </SimpleGrid>
         </>
     )
 }
