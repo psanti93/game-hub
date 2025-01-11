fca092b0b345b5dd4e4323c438c9ff3d5317ec03
psanti93 <paulsantiago282@gmail.com>
2025-01-11 18:23:19 -0500

    add critic score to game card

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -13,6 +13,7 @@ export interface Game {
     name: string
     background_image: string
     parent_platforms: {platform:Platform}[] // array of objects of type platform
+    metacritic: number
 }
 
 interface FetchGamesResponse {
