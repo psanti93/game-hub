e64d7023e5e35d8fd4dda6c20c95679a7c602682
psanti93 <paulsantiago282@gmail.com>
2025-01-10 22:05:39 -0500

    add reponsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import {Button, HStack} from "@chakra-ui/react";
+import {Grid, GridItem, Show} from "@chakra-ui/react";
 
 const App = () => {
     return(
-        <HStack>
-            <Button colorPalette={"blue"}>Click Me</Button>
-        </HStack>
+        <Grid templateAreas={{
+            base: `"nav" "main"`, // for smaller devices - mobile device
+            lg: `"nav nav" "aside main"`, // for bigger devices
+
+        }}>
+            <GridItem area={'nav'} bg={'coral'}> Nav </GridItem>
+            {/*only show on devices bigger than a mobile device*/}
+            <Show above={"lg"}>
+                <GridItem area={'aside'} bg={'gold'}> Aside </GridItem>
+            </Show>
+            <GridItem area={'main'} bg={'dodgerblue'}>Main</GridItem>
+        </Grid>
     )
 }
 
