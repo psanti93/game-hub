737790b31834c530e3a9f353e963a76946fd4893
psanti93 <paulsantiago282@gmail.com>
2025-01-12 00:23:25 -0500

    add templateColumns to fix width for aside column, and padding to even out genre list

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,22 @@ import GenreList from "./components/GenreList.tsx";
 
 const App = () => {
     return(
-        <Grid templateAreas={{
+        <Grid
+            templateAreas={{
             base: `"nav" "main"`, // for smaller devices - mobile device
             lg: ` "nav nav"  "aside main"`, // for bigger devices //this determines the order too of how the items are set
 
-        }}>
-            <GridItem area={'nav'}> <NavBar/> </GridItem>
+        }}
+        templateColumns={{   // applies a fix width for our aside column
+            base: '1fr',
+            lg: '200px 1fr'
+        }}
 
+        >
+            <GridItem area={'nav'}> <NavBar/> </GridItem>
             {/*only show on devices bigger than a mobile device*/}
             <Show above={"lg"}>
-                <GridItem area={'aside'}> <GenreList/> </GridItem>
+                <GridItem area={'aside'} paddingX={5}> <GenreList/> </GridItem>
             </Show>
             <GridItem area={'main'} >
                 <GameGrid/>
