ce8ade2952bdcf6d31187c02e3db58191fc438dd
psanti93 <paulsantiago282@gmail.com>
2025-01-18 16:26:44 -0500

    Add emojis to game cards

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -13,7 +13,8 @@ export interface Game {
     name: string
     background_image: string
     parent_platforms: {platform:Platform}[] // array of objects of type platform
-    metacritic: number
+    metacritic: number;
+    rating_top: number; // whole number
 }
 
 
